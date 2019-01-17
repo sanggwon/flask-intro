1c37ce3dfa9d0ab585d1a4c18b4eb629a55dc37c
Sang Gwon Cha <ck2570@naver.com>
2019-01-17 08:51:59 +0000

    D12 | 190117 | csv

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
 import random
+import csv
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/") # /하나 있는거는 내 컴퓨터
@@ -50,5 +55,42 @@ def naver():
 def google():
     return render_template("google.html")
     
+@app.route("/flex")
+def flex():
+    return render_template("flex.html")
+
+@app.route("/grid")
+def grid():
+    return render_template("grid.html")
+    
+@app.route("/opgg")
+def opgg():
+    return render_template("opgg.html")
+
+@app.route("/summoner")
+def summoner():
+    # 검색하려고 하는 소환사 이름
+    username = request.args.get("summoner")
+    # 실제 opgg사이트에서 검색요청 url
+    url = "http://www.op.gg/summoner/userName="
+    res = requests.get(url+username).text
+    soup = BeautifulSoup(res, "html.parser")
+    win = soup.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins").text
+    lose = soup.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.losses").text
+    rank = soup.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierRank > span").text
+
+    f= open("output.csv", "a+", encoding="utf-8", newline="")
+    csv_f = csv.writer(f)
+    csv_f.writerow([username, win, lose, rank, datetime.datetime.now()])
+    f.close()
+    return render_template("summoner.html", username=username, lose = lose, win = win, rank=rank)
+    
+
+@app.route("/rank")
+def rank():
+    f = open("output.csv","r",encoding="utf-8")
+    csv_r = csv.reader(f)
+    return render_template("rank.html",csv_r=csv_r)
+
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
